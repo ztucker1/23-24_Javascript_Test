06976f669d93f87f0daddd8b416f0fe413fc853f
13433 <natalie.tucker@vtsd.com>
2024-01-12 13:52:33 -0500

    Changes by 16075

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,10 @@
 /* Put code for the task below */
+//FINISH
+function imgHover(){
+    const buttonTask1 = document.getElementById("buttonTask1")
+    buttonTask1.style.display = "block";
+}
+function buttonT1(){
+    const imageTask1 = document.getElementById("imageTask1")
+    imageTask1.style.display = "none";
+}
--- assets/javascript/task4.js
+++ assets/javascript/task4.js
@@ -1,1 +1,11 @@
 /* Put code for the task below */
+//Create a button within the navigation called "clear" which opens a new page with the text "new page has been created".
+// I think this is done
+function task4() {
+    const buttonTask4 = document.getElementById("buttonTask4");
+    buttonTask4.innerHTML = "Clear";
+    buttonTask4.addEventListener("click", function () {
+        window.open("http://localhost:63342/23-24_Javascript_Test/Task4.html?_ijt=hjpdli0r87gu6mgf303eblt1gk", "_blank");
+    });
+    document.getElementById("nav").appendChild("clear");
+}
